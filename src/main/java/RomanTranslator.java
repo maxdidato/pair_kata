55fe602e45913e2581ea745ac9c08058c938ff8c
maxdidato <maxdidato>
2017-11-21 23:06:16 +0000

    first commit

---
+++ src/main/java/RomanTranslator.java
@@ -0,0 +1,6 @@
+public class RomanTranslator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/RomanTranslatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RomanTranslatorTest {
+    @Test
+    public void test_it() {
+        Assert.fail("Implement Me");
+    }
+
+}
